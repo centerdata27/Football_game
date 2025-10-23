55cfe1182ee6053625c9cc0c9650e649cc7a3ccb
Kevin De La CRuz <0KEVINDELACRUZ0@GMAIL.COM>
2025-10-23 19:02:41 -0400

    progress

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -1,6 +1,22 @@
 import type{ Score, Team} from "../types"
-
 interface ScoreBoardProps {
     score: Score,
-    posseession: Team,
+    possession: Team,
 }
+
+export default function ScoreBoard ({score, possession}: ScoreBoardProps){
+return(
+    <section className = "bg-white rounded-lg shadow p-4 w-full max-w-xl">
+        <h2 className= 'text-xl font bold text-gray-800'>ScoreBoard</h2>
+        <div className="flex justify-between text-lg font-semibold text-gray-700">
+            <span>Home:{score.HOME}</span>
+             <span>Away:{score.AWAY}</span>
+        </div>
+        <p className="mt-2 text-sm text-gray-600">
+            Possession: {''}
+            <span className="font-bold text-indigo-600">{possession}</span>
+        </p>
+    </section>
+)
+}
+
